092f282d1b5555280ad8f65d8fb5d5fdb5f5fab4
dengnanhao <dengnanhao@hddata.cn>
2023-02-17 17:04:13 +0800

    style(blog): 详情页样式调整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,20 @@ const Header = () => {
   // 渲染静态菜单
   const _renderStaticMenus = () => {
     return menus.map((menu) => (
-      <li
+      <div
         className="px-4 py-2 text-sm border-b-transparent border-b  hover:border-gray-700"
         key={menu.id}
       >
         <a className="w-full h-full" href={menu.path}>
           {menu.name}
         </a>
-      </li>
+      </div>
     ));
   };
   return (
     <header className="h-16 bg-white px-6 group cursor-pointer dark:bg-gray-800 shadow-md">
       <div className="w-full h-16 flex justify-center items-center">
-        <h2 className="dark:text-white">{siteTitle}</h2>
+        <div className="dark:text-white">{siteTitle}</div>
       </div>
       <div
         className="flex items-center justify-between h-16 
@@ -27,10 +27,10 @@ const Header = () => {
         px-6 bg-white 
         dark:bg-gray-800"
       >
-        <h2 className="dark:text-white">{siteTitle}</h2>
-        <ul className="flex items-center dark:text-white">
+        <div className="dark:text-white">{siteTitle}</div>
+        <div className="flex items-center dark:text-white">
           {_renderStaticMenus()}
-        </ul>
+        </div>
       </div>
     </header>
   );
--- src/components/ui/blog/Markdown.tsx
+++ src/components/ui/blog/Markdown.tsx
@@ -1,6 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-
+import "../../../styles/base.css";
 interface IProps {
   markdown: string;
 }
